b7a9980721a3ec6d0b6c4abbe751322597560bb2
Alexandre <alexandre.reynaud@custom-dev.fr>
2021-02-23 15:10:55 +0100

    commit part6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,14 +3,19 @@ import { Http} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators/map';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl= '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id:number) : Observable<Album> {
     return this._http.get(this._albumUrl).pipe(map(response => <Album>response.json()))
+  }
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).pipe(map(response => <Product[]>response.json()));
   }
 }
